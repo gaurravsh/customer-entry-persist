0daf6e2c3fce9dde368f35a37d8fe6ebd16023c8
gauravsharma <21275839+gaurravsh@users.noreply.github.com>
2023-08-27 21:53:51 +0530

    updated Lambda Handler Signature

--- app/src/main/scala/dev/shaga/jackit/lambda/handler/LambdaHandler.scala
+++ app/src/main/scala/dev/shaga/jackit/lambda/handler/LambdaHandler.scala
@@ -1,12 +1,12 @@
 package dev.shaga.jackit.lambda.handler
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import dev.shaga.jackit.lambda.process.ProcessInput
 
 
-object LambdaHandler extends RequestHandler[java.util.Map[String, String], String]{
-  override def handleRequest(input: java.util.Map[String, String], context: Context): String = {
+object LambdaHandler extends RequestHandler[java.util.Map[String, String], Unit]{
+  override def handleRequest(input: java.util.Map[String, String], context: Context): Unit = {
     val body = input.get("body")
-
-    null
+    ProcessInput.processJson(body)
   }
 }
